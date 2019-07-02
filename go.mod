19256f6b9941d76dfe2a8d7de0f34a681196283e
ChronosX88 <chronosx88@gmail.com>
2019-07-02 13:51:51 +0400

    [WIP] feat: Port of p2chat as golang library for Android

--- go.mod
+++ go.mod
@@ -1,3 +1,27 @@
 module github.com/MoonSHRD/p2chat-android
 
 go 1.12
+
+require (
+	github.com/MoonSHRD/p2chat v1.0.0
+	github.com/btcsuite/btcd v0.0.0-20190629003639-c26ffa870fd8 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/deckarep/golang-set v1.7.1
+	github.com/libp2p/go-eventbus v0.0.3 // indirect
+	github.com/libp2p/go-libp2p v0.2.0
+	github.com/libp2p/go-libp2p-core v0.0.6
+	github.com/libp2p/go-libp2p-pubsub v0.1.0
+	github.com/libp2p/go-libp2p-swarm v0.1.1 // indirect
+	github.com/libp2p/go-msgio v0.0.4 // indirect
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/miekg/dns v1.1.15 // indirect
+	github.com/multiformats/go-multiaddr v0.0.4
+	github.com/multiformats/go-multihash v0.0.6 // indirect
+	github.com/opentracing/opentracing-go v1.1.0 // indirect
+	go.opencensus.io v0.22.0 // indirect
+	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
+	golang.org/x/net v0.0.0-20190628185345-da137c7871d7 // indirect
+	golang.org/x/sync v0.0.0-20190423024810-112230192c58 // indirect
+	golang.org/x/sys v0.0.0-20190626221950-04f50cda93cb // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
+)
---
+++ pkg/main.go
@@ -0,0 +1,239 @@
+package pkg
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/json"
+	"fmt"
+	"sync"
+
+	"github.com/MoonSHRD/p2chat-android/pkg/utils"
+	"github.com/MoonSHRD/p2chat/api"
+	p2chat "github.com/MoonSHRD/p2chat/pkg"
+	mapset "github.com/deckarep/golang-set"
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/crypto"
+	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/peerstore"
+	"github.com/libp2p/go-libp2p-core/protocol"
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	"github.com/multiformats/go-multiaddr"
+)
+
+type Config struct {
+	RendezvousString string // Unique string to identify group of nodes. Share this with your friends to let them connect with you
+	ProtocolID       string // Sets a protocol id for stream headers
+	ListenHost       string // The bootstrap node host listen address
+	ListenPort       int    // Node listen port
+}
+
+var myself host.Host
+
+var globalCtx context.Context
+var globalCtxCancel context.CancelFunc
+
+var pbMutex sync.Mutex
+var Pb *pubsub.PubSub
+var networkTopics mapset.Set
+var messageQueue utils.Queue
+var handler p2chat.Handler
+var serviceTopic string
+
+// this function get new messages from subscribed topic
+func readSub(subscription *pubsub.Subscription, incomingMessagesChan chan pubsub.Message) {
+	ctx := globalCtx
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+		}
+		msg, err := subscription.Next(context.Background())
+		if err != nil {
+			fmt.Println("Error reading from buffer")
+			panic(err)
+		}
+
+		if string(msg.Data) == "" {
+			return
+		}
+		if string(msg.Data) != "\n" {
+			addr, err := peer.IDFromBytes(msg.From)
+			if err != nil {
+				fmt.Println("Error occurred when reading message From field...")
+				panic(err)
+			}
+
+			// This checks if sender address of incoming message is ours. It is need because we get our messages when subscribed to the same topic.
+			if addr == myself.ID() {
+				continue
+			}
+			incomingMessagesChan <- *msg
+		}
+
+	}
+}
+
+// Publish message into some topic
+func PublishMessage(topic string, text string) {
+	message := &api.BaseMessage{
+		Body: text,
+		Flag: api.FLAG_GENERIC_MESSAGE,
+	}
+
+	sendData, err := json.Marshal(message)
+	if err != nil {
+		fmt.Println("Error occurred when marshalling message object")
+		return
+	}
+
+	pbMutex.Lock()
+	err = Pb.Publish(topic, sendData)
+	pbMutex.Unlock()
+	if err != nil {
+		fmt.Println("Error occurred when publishing")
+		return
+	}
+}
+
+func Start(rendezvous *string, pid *string, listenHost *string, port *int) {
+	cfg := GetConfig(rendezvous, pid, listenHost, port)
+	serviceTopic = cfg.RendezvousString
+
+	fmt.Printf("[*] Listening on: %s with port: %d\n", cfg.ListenHost, cfg.ListenPort)
+
+	ctx, ctxCancel := context.WithCancel(context.Background())
+	globalCtx = ctx
+	globalCtxCancel = ctxCancel
+	r := rand.Reader
+
+	// Creates a new RSA key pair for this host.
+	prvKey, _, err := crypto.GenerateKeyPairWithReader(crypto.RSA, 2048, r)
+	if err != nil {
+		panic(err)
+	}
+
+	// 0.0.0.0 will listen on any interface device.
+	sourceMultiAddr, _ := multiaddr.NewMultiaddr(fmt.Sprintf("/ip4/%s/tcp/%d", cfg.ListenHost, cfg.ListenPort))
+
+	// libp2p.New constructs a new libp2p Host.
+	// Other options can be added here.
+	host, err := libp2p.New(
+		ctx,
+		libp2p.ListenAddrs(sourceMultiAddr),
+		libp2p.Identity(prvKey),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Set a function as stream handler.
+	// This function is called when a peer initiates a connection and starts a stream with this peer. (Handle incoming connections)
+	//	host.SetStreamHandler(protocol.ID(cfg.ProtocolID), handleStream)
+
+	fmt.Printf("\n[*] Your Multiaddress Is: /ip4/%s/tcp/%v/p2p/%s\n", cfg.ListenHost, cfg.ListenPort, host.ID().Pretty())
+
+	myself = host
+
+	// Initialize pubsub object
+	pb, err := pubsub.NewFloodsubWithProtocols(context.Background(), host, []protocol.ID{protocol.ID(cfg.ProtocolID)}, pubsub.WithMessageSigning(false))
+	if err != nil {
+		fmt.Println("Error occurred when create PubSub")
+		panic(err)
+	}
+
+	Pb = pb
+
+	handler = p2chat.NewHandler(pb, serviceTopic, &networkTopics)
+
+	peerChan := p2chat.InitMDNS(ctx, host, serviceTopic)
+
+	//Subscription should go BEFORE connections
+	// NOTE:  here we use Randezvous string as 'topic' by default .. topic != service tag
+	subscription, err := pb.Subscribe(serviceTopic)
+	if err != nil {
+		fmt.Println("Error occurred when subscribing to topic")
+		panic(err)
+	}
+
+	incomingMessages := make(chan pubsub.Message)
+	go readSub(subscription, incomingMessages)
+	go getNetworkTopics()
+
+MainLoop:
+	for {
+		select {
+		case <-ctx.Done():
+			break MainLoop
+		case msg := <-incomingMessages:
+			{
+				handler.HandleIncomingMessage(msg, func(textMessage p2chat.TextMessage) {
+					messageQueue.PushFront(textMessage)
+				})
+			}
+		case newPeer := <-peerChan:
+			{
+				fmt.Println("\nFound peer:", newPeer, ", add address to peerstore")
+
+				// Adding peer addresses to local peerstore
+				host.Peerstore().AddAddr(newPeer.ID, newPeer.Addrs[0], peerstore.PermanentAddrTTL)
+				// Connect to the peer
+				if err := host.Connect(ctx, newPeer); err != nil {
+					fmt.Println("Connection failed:", err)
+				}
+				fmt.Println("\nConnected to:", newPeer)
+			}
+		}
+	}
+}
+
+// TODO: get this part to separate file (flags or whatever). all defaults parameters and their parsing should be done from separate file
+func GetConfig(rendezvous *string, pid *string, host *string, port *int) *Config {
+	c := &Config{}
+
+	if *rendezvous != "" && rendezvous != nil {
+		c.RendezvousString = *rendezvous
+	} else {
+		c.RendezvousString = "moonshard"
+	}
+
+	if *pid != "" && pid != nil {
+		c.ProtocolID = *pid
+	} else {
+		c.ProtocolID = "/chat/1.1.0"
+	}
+
+	if *host != "" && host != nil {
+		c.ListenHost = *host
+	} else {
+		c.ListenHost = "0.0.0.0"
+	}
+
+	if *port != 0 && port != nil && !(*port < 0) && !(*port > 65535) {
+		c.ListenPort = *port
+	} else {
+		c.ListenPort = 4001
+	}
+
+	return c
+}
+
+func getNetworkTopics() {
+	ctx := globalCtx
+	handler.RequestNetworkTopics(ctx)
+}
+
+func GetMessages() *string {
+	textMessage := messageQueue.PopBack()
+	if textMessage != nil {
+		jsonData, err := json.Marshal(textMessage)
+		if err != nil {
+			return nil
+		}
+		str := string(jsonData)
+		return &str
+	}
+	return nil
+}
