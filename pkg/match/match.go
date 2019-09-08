5abe41d690c230a1d6f245c937ab17c282d011a1
alyakimenko <urlucidfall@gmail.com>
2019-09-08 10:22:28 +0300

    feat: update match response type

--- pkg/match/match.go
+++ pkg/match/match.go
@@ -1,7 +1,5 @@
 package match
 
-import "github.com/libp2p/go-libp2p-core/peer"
-
 // Response alias is used for json marshaling
 // the match ([topic]=>[mxIDs]) response
-type Response map[string][]peer.ID
+type Response map[string][]string
