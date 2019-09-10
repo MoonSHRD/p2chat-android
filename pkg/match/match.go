c00dd9603c8fece1d12493d2bb233a85109c6f02
alyakimenko <urlucidfall@gmail.com>
2019-09-10 20:19:18 +0300

    feat: add MatchProcessor type to work with matches

--- pkg/match/match.go
+++ pkg/match/match.go
@@ -1,5 +1,35 @@
 package match
 
+import (
+	"github.com/MoonSHRD/p2chat-android/pkg/utils"
+)
+
 // Response alias is used for json marshaling
-// the match ([topic]=>[mxIDs]) response
+// the whole match ([topic]=>[matrixIDs]) response
+// or for the new matches ([matrixID]=>[topics])
 type Response map[string][]string
+
+// MatchProcessor is helper type for work
+// with match operations processing
+type MatchProcessor struct {
+	mathes          Response
+	newMatchesQueue utils.Queue
+}
+
+// GetAllMatches returns the whole matches map within json format
+func (mc *MatchProcessor) GetAllMatches() string {
+	return utils.ObjectToJSON(mc.mathes)
+}
+
+// GetNewMatch returns new match from the matches-queue
+func (mc *MatchProcessor) GetNewMatch() string {
+	return utils.ObjectToJSON(mc.newMatchesQueue.PopBack())
+}
+
+// AddNewMatch pushes new match [topic]=>[newMatrixID] to the matches-queue and map
+func (mc *MatchProcessor) AddNewMatch(matrixID string, topics []string) {
+	for _, topic := range topics {
+		mc.mathes[topic] = append(mc.mathes[topic], matrixID)
+	}
+	mc.newMatchesQueue.PushBack(Response{matrixID: topics})
+}
