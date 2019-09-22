236fedaff8c30e97b5077489ac5c12c57aac6f3b
ChronosX88 <chronosx88@gmail.com>
2019-09-23 02:48:35 +0400

    feat: Implement new match system

--- pkg/match/match.go
+++ pkg/match/match.go
@@ -4,16 +4,25 @@ import (
 	"github.com/MoonSHRD/p2chat-android/pkg/utils"
 )
 
-// Response alias is used for json marshaling
-// the whole match ([topic]=>[matrixIDs]) response
-// or for the new matches ([matrixID]=>[topics])
-type Response map[string][]string
+type Match struct {
+	PeerID   string `json:"peerID"`
+	MatrixID string `json:"matrixID"`
+	Topic    string `json:"topic"`
+	IsValid  bool   `json:"isValid"`
+}
 
 // MatchProcessor is helper type for work
 // with match operations processing
 type MatchProcessor struct {
-	mathes          Response
-	newMatchesQueue utils.Queue
+	mathes          map[string][]Match
+	newMatchesQueue *utils.Queue
+}
+
+func NewMatchProcessor() *MatchProcessor {
+	return &MatchProcessor{
+		mathes:          make(map[string][]Match),
+		newMatchesQueue: utils.New(),
+	}
 }
 
 // GetAllMatches returns the whole matches map within json format (or empty string, if we haven't any matches)
@@ -36,9 +45,34 @@ func (mp *MatchProcessor) GetNewMatch() string {
 }
 
 // AddNewMatch pushes new match [topic]=>[newMatrixID] to the matches-queue and map
-func (mp *MatchProcessor) AddNewMatch(matrixID string, topics []string) {
-	for _, topic := range topics {
-		mp.mathes[topic] = append(mp.mathes[topic], matrixID)
+func (mp *MatchProcessor) AddNewMatch(topic string, peerID string, matrixID string) {
+	match := Match{
+		PeerID:   peerID,
+		MatrixID: matrixID,
+		Topic:    topic,
+		IsValid:  true,
 	}
-	mp.newMatchesQueue.PushBack(Response{matrixID: topics})
+	mp.mathes[topic] = append(mp.mathes[topic], match)
+	mp.newMatchesQueue.PushBack(match)
+}
+
+func (mp *MatchProcessor) RemoveMatch(topic string, peerID string, matrixID string) {
+	for i := 0; i < len(mp.mathes[topic]); i++ {
+		if mp.mathes[topic][i].PeerID == peerID && mp.mathes[topic][i].MatrixID == matrixID {
+			mp.removeMatch(topic, i)
+			break
+		}
+	}
+
+	invalidMatchEvent := Match{
+		PeerID:   peerID,
+		MatrixID: matrixID,
+		Topic:    topic,
+		IsValid:  false,
+	}
+	mp.newMatchesQueue.PushBack(invalidMatchEvent)
+}
+
+func (mp *MatchProcessor) removeMatch(topic string, index int) {
+	mp.mathes[topic] = append(mp.mathes[topic][:index], mp.mathes[topic][index+1:]...)
 }
